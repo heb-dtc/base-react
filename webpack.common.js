d7fff8ecbfa6593d0be6d08bc825ab4c6ffb1616
Florent NOEL <florent.noel@protonmail.com>
2020-09-25 17:37:19 +0200

    Add css modules support
    Install style-loader and css-loader for processing css files.
    Add style.css with some basic css.
    Import css file in App component.

--- webpack.common.js
+++ webpack.common.js
@@ -9,7 +9,20 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ['babel-loader', 'eslint-loader'],
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+        ],
       },
     ],
   },
